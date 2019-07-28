fd98db4c9477e76f27d665ba0012aadc14e2c978
Akshay <akshayyadav7613@gmail.com>
2019-07-28 17:21:41 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'transections.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transections> transection = [
+    Transections(
+        id: "1", title: "buy shoes ", money: 250.5, date: DateTime.now()),
+    Transections(
+        id: "1", title: "buy banana ", money: 20.0, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+                width: double.infinity,
+                child: Card(
+                  child: Text("seeee"),
+                )),
+            Column(
+                children: transection.map((tx) {
+              return Card(
+                  child: ListTile(
+                title: Text(tx.title),
+                subtitle: Text(tx.date.toString()),
+                    leading: Text(tx.money.toString()),
+              ));
+            }).toList())
+          ],
+        ));
+  }
+}
---
+++ lib/transections.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+class Transections {
+  final String id;
+  final String title;
+  final double money;
+  final DateTime date;
+
+  Transections(
+      {@required this.id,
+      @required this.title,
+      @required this.money,
+      @required this.date});
+}
